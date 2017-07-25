96e073d7376d490800783c100df4d4759e78baeb
ntdat2207 <ntdat2207@gmail.com>
2017-07-25 14:19:51 +0700

    update 1

--- app/src/main/java/com/example/administrator/freakingmathfull/MainActivity.java
+++ app/src/main/java/com/example/administrator/freakingmathfull/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.administrator.freakingmathfull;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnPlay, btnHighScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        btnPlay.setOnClickListener(this);
+        btnHighScore.setOnClickListener(this);
+    }
+
+    private void initView() {
+        btnPlay =(Button) findViewById(R.id.btnPlay);
+        btnHighScore = (Button) findViewById(R.id.btnHighScore);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.btnPlay:
+                Toast.makeText(this, "PLAY", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnHighScore:
+                Toast.makeText(this, "HIGH", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
